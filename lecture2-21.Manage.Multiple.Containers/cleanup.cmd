9513e489af48ac6b0808b9ff23e6f96c0d7f22ee
Adrian Farnell <adrianjfarnell@gmail.com>
2018-01-29 16:34:24 +0000

    ....and the corresponding cleanup batch file

---
+++ lecture2-21.Manage.Multiple.Containers/cleanup.cmd
@@ -0,0 +1,9 @@
+docker container stop mysql
+docker container stop httpd
+docker container stop nginx
+
+docker container rm mysql
+docker container rm httpd
+docker container rm nginx
+
+docker container ls -al
